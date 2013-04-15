4b3adf151a797dcda5eb43022359d95d6c99d95e
Hack Reactor Students <accounts@hackreactor.com>
2013-04-14 17:29:58 -0700

    basic working app

--- LunchRoulette.js
+++ LunchRoulette.js
@@ -1,21 +1,22 @@
-if (Meteor.isClient) {
+  // var FridayPool = new Meteor.Collection('FridayPool');
+  // var SaturdayPool = new Meteor.Collection('SaturdayPool');
+  // var SundayPool = new Meteor.Collection('SundayPool');
+  // var MondayPool = new Meteor.Collection('MondayPool');
+  // var TuesdayPool = new Meteor.Collection('TuesdayPool');
+  // var WednesdayPool = new Meteor.Collection('WednesdayPool');
 
   var peoplePool = new Meteor.Collection('peoplePool');
-  var groupPool = new Meteor.Collection('groupPool');
-  var lunchGroups = new Meteor.Collection('lunchGroups');
-  var peopleArray;
 
-  var groupPeople = function(array) {
-    // var groups = [];
-    for (var i = 0; i < array.length; i += 3) {
-      console.log((array.slice(i, i + 3)));
-    }
-    // if (groups[groups.length - 1].length === 1) {
-    //   groups[0].push(groups.pop());
-    // }
+  var Groups = new Meteor.Collection('Groups');
 
-    return groups;
-  };
+  var date = new Date();
+  var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  var today = weekdays[date.getDay()];
+  var yesterday = date.getDay() > 0 ? weekdays[date.getDay() - 1] : weekdays[6];
+
+
+if (Meteor.isClient) {
 
   Template.signUp.events({
     'click .submit' : function (e) {
@@ -23,45 +24,78 @@ if (Meteor.isClient) {
       var user = {};
       user.name = $('.name').val();
       user.email = $('.email').val();
-      user.phone = $('.phone').val();      
-      peoplePool.insert(user);
+      user.phone = $('.phone').val();
+      user.lunchDay = $('.day').val();
+      user.lunchDay === '%s' ? alert('Please pick a day') :
 
+      peoplePool.insert(user);
       $('form > input').val('');
       e.preventDefault();
     }
   });
 
-  Template.list.peopleInPool = function() {
-    return peoplePool.find({});
+  Template.FridayList.FridayPool = function() {
+    return peoplePool.find({lunchDay : 'Friday'});
   };
 
-  Template.grouplist.groupsMade = function() {
-    return groupPool.find({});
+  Template.SaturdayList.SaturdayPool = function() {
+    return peoplePool.find({lunchDay: 'Saturday'});
   };
 
-  Meteor.autorun(function() {
-    peopleArray = peoplePool.find({}).fetch();
-    if (peopleArray.length !== 0) {
-      groups = groupPeople(peopleArray);
-      for (var i = 0; i < groups.length; i++) {
-        var group = {};
-        group.number = i;
-        group.people = groups[i];
+  Template.SundayList.SundayPool = function() {
+    return peoplePool.find({lunchDay: 'Sunday'});
+  };
 
-        groupPool.insert(group);
-      }
-    }
-  });
+  Template.MondayList.MondayPool = function() {
+    return peoplePool.find({lunchDay: 'Monday'});
+  };
 
+  Template.TuesdayList.TuesdayPool = function() {
+    return peoplePool.find({lunchDay: 'Tuesday'});
+  };
+
+  Template.WednesdayList.WednesdayPool = function() {
+    console.log(peoplePool.find({lunchDay: 'Wednesday'}))
+    return peoplePool.find({lunchDay: 'Wednesday'});
+  };
+
+  Template.TodayList.TodayPool = function() {
+    // todayArray = peoplePool.find({lunchDay: today});
+
+  };
+
+  // Meteor.subscribe('peoplePool', function() {
+  Template.calendar.rendered = function(){
+        console.log(today, yesterday);
+        $('#' + today).addClass('hiddenday');
+        $('#' + yesterday).removeClass('hiddenday');
+        // peoplePool.remove({lunchDay: yesterday});
+    };
+//   });
 }
- 
+
 if (Meteor.isServer) {
-  var peoplePool = new Meteor.Collection('peoplePool');
-  var groupPool = new Meteor.Collection('groupPool');
+
+  // for item in peoplePool.find({lunchDay: today}) {
+  //   console.log
+  // }
+  console.log(peoplePool.findOne({lunchDay: today}));
+
+  // var now = new Date();
+  // var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
+  // console.log(millisTill10)
+  // if (millisTill10 < 0) {
+  //      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+  // }
+
+  // setTimeout(function(){/*set function(
+
+  //   create groups --> helper function
+  //   send notifications -- helper function
+  //   */
+  // }, millisTill8);
+
   Meteor.methods({
-    'superclear' : function() {
-      peoplePool.remove({});
-      groupPool.remove({});
-    }
+
   });
 }
