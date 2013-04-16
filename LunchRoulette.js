60878e0618856791b73dac0edb58cb52061a74d8
Hack Reactor Students <accounts@hackreactor.com>
2013-04-15 21:25:41 -0700

    basic working application.  huzzah.

--- LunchRoulette.js
+++ LunchRoulette.js
@@ -1,9 +1,3 @@
-  // var FridayPool = new Meteor.Collection('FridayPool');
-  // var SaturdayPool = new Meteor.Collection('SaturdayPool');
-  // var SundayPool = new Meteor.Collection('SundayPool');
-  // var MondayPool = new Meteor.Collection('MondayPool');
-  // var TuesdayPool = new Meteor.Collection('TuesdayPool');
-  // var WednesdayPool = new Meteor.Collection('WednesdayPool');
 
   var peoplePool = new Meteor.Collection('peoplePool');
 
@@ -26,10 +20,9 @@ if (Meteor.isClient) {
       user.email = $('.email').val();
       user.phone = $('.phone').val();
       user.lunchDay = $('.day').val();
-      user.lunchDay === '%s' ? alert('Please pick a day') :
-
-      peoplePool.insert(user);
+      user.lunchDay === '%s' ? alert('Please pick a day') : peoplePool.insert(user);
       $('form > input').val('');
+      $('form .day').val('');
       e.preventDefault();
     }
   });
@@ -55,23 +48,33 @@ if (Meteor.isClient) {
   };
 
   Template.WednesdayList.WednesdayPool = function() {
-    console.log(peoplePool.find({lunchDay: 'Wednesday'}))
     return peoplePool.find({lunchDay: 'Wednesday'});
   };
 
   Template.TodayList.TodayPool = function() {
-    // todayArray = peoplePool.find({lunchDay: today});
+    var names = [];
 
+    var todayGrps = Groups.find().fetch();
+    for(var key in todayGrps){
+      for(var k in todayGrps[key]){
+        if (typeof todayGrps[key][k] === 'string'){
+          names.push({name: 'Lunch Group:'})
+        } else {
+          names.push(todayGrps[key][k]);
+        }
+
+      }
+    }
+    return names.reverse();
   };
 
   // Meteor.subscribe('peoplePool', function() {
   Template.calendar.rendered = function(){
-        console.log(today, yesterday);
-        $('#' + today).addClass('hiddenday');
-        $('#' + yesterday).removeClass('hiddenday');
-        // peoplePool.remove({lunchDay: yesterday});
-    };
+    $('#' + today).addClass('hiddenday');
+    $('#' + yesterday).removeClass('hiddenday');
+  };
 //   });
+
 }
 
 if (Meteor.isServer) {
@@ -79,23 +82,71 @@ if (Meteor.isServer) {
   // for item in peoplePool.find({lunchDay: today}) {
   //   console.log
   // }
-  console.log(peoplePool.findOne({lunchDay: today}));
 
-  // var now = new Date();
-  // var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
-  // console.log(millisTill10)
-  // if (millisTill10 < 0) {
-  //      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
-  // }
 
-  // setTimeout(function(){/*set function(
+  var shuffle = function ( myArray ) {
+    var i = myArray.length;
+    if ( i === 0 ) return false;
+    while ( --i ) {
+       var j = Math.floor( Math.random() * ( i + 1 ) );
+       var tempi = myArray[i];
+       var tempj = myArray[j];
+       myArray[i] = tempj;
+       myArray[j] = tempi;
+    }
+    return myArray;
+  };
+
+  Meteor.setInterval(function(){
+    console.log('its time');
+    var peeps = peoplePool.find({lunchDay: today}).fetch();
+    var groupSize = 3; // this is the ordinary size of a lunch group
+    var now = new Date();
+    console.log(now.getHours(), now.getMinutes());
+
+    var peepGroup;
+
+    if (now.getHours() === 12 && now.getMinutes() === 00) {
+      console.log('working');
+      Groups.remove();
+      peeps = shuffle(peeps);
+      for (var i = 0; i < peeps.length; i += groupSize) {
+        if (peeps.length - i === groupSize + 1) {
+          peepGroup = peeps.slice(i, peeps.length);
+          peoplePool.remove({lunchDay: today});
+          return Groups.insert(peepGroup);
+        } else {
+          peepGroup = peeps.slice(i, i+groupSize);
+          Groups.insert(peepGroup);
+        }
+      }
+      console.log(Groups.find({}).fetch());
+      return Groups;
+    }
+  }, 60000);
+
+  // console.log("this is the peoplePool", peoplePool.find({lunchDay: today}).fetch());
+  console.log(Groups.find({}).fetch());
+
+};
+  // TODO: assign a random groupNumber to _.each person in the peoplePool...
+      // constrain the number to math.floor(peeps.length / 3)
+      // render to the DOM based on that random number (extra credit: notify them by SMS)
+
+
+//     var number = Math.floor((Math.random() * peeps.length / 3)sa + 1);
+//     peoplePool.update({lunchDay: today}, { $push : {groupID : number}});  // TODO: fix this
+
+
+
+
+
 
   //   create groups --> helper function
   //   send notifications -- helper function
   //   */
   // }, millisTill8);
 
-  Meteor.methods({
+  // Meteor.methods({
 
-  });
-}
+  // });
